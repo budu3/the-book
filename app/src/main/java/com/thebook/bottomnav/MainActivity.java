04e30e9d9e722f0490b46aa05e98d3f318e13bce
budu3 <budu3@yahoo.com>
2024-02-11 17:49:54 -0600

    Add explicit and explicit intent code

--- app/src/main/java/com/thebook/bottomnav/MainActivity.java
+++ app/src/main/java/com/thebook/bottomnav/MainActivity.java
@@ -1,6 +1,9 @@
 package com.thebook.bottomnav;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,5 +20,23 @@ public class MainActivity extends AppCompatActivity {
         webView = (WebView) findViewById(R.id.WebView1);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("file:///android_asset/index.html");
+
+        findViewById(R.id.explicit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("message","Message sent from MainActivity");
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.implicit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.example.com"));
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/thebook/bottomnav/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.thebook.bottomnav;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        //retrieve your message from the intent
+        String message = getIntent().getStringExtra("message");
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
