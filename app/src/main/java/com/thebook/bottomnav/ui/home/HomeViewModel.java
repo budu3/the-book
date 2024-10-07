93f959a6d553301a93af430b0ec0001dd8ebebe6
budu3 <budu3@yahoo.com>
2024-10-07 10:42:06 -0500

    Modify HomeViewModel.java to reduce long lines

--- app/src/main/java/com/thebook/bottomnav/ui/home/HomeViewModel.java
+++ app/src/main/java/com/thebook/bottomnav/ui/home/HomeViewModel.java
@@ -4,13 +4,9 @@ import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
-
-import java.util.Map;
-
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
 //You need to extend AndroidViewModel
 public class HomeViewModel extends AndroidViewModel {
@@ -23,7 +19,8 @@ public class HomeViewModel extends AndroidViewModel {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
 
-        SharedPreferences prefs = getApplication().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences prefs = getApplication()
+          .getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         String movieStr = prefs.getString("movie1","");
 
         mText.setValue(movieStr);
