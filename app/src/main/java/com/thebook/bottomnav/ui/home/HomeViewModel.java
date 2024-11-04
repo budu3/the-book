d8f933364b8af98aa481c3213fec7a13e253ea14
budu3 <budu3@yahoo.com>
2024-11-03 21:32:22 -0600

    Add word wrap to HomeViewModel.java

--- app/src/main/java/com/thebook/bottomnav/ui/home/HomeViewModel.java
+++ app/src/main/java/com/thebook/bottomnav/ui/home/HomeViewModel.java
@@ -4,22 +4,16 @@ import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.Map;
-
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
 //You need to extend AndroidViewModel
 public class HomeViewModel extends AndroidViewModel {
-
     private MutableLiveData<String> mText;
     private MutableLiveData<ArrayList<SimpleViewModel>> movieLiveData;
     private ArrayList<SimpleViewModel> movieList;
@@ -32,27 +26,30 @@ public class HomeViewModel extends AndroidViewModel {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
 
-        SharedPreferences prefs = getApplication().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences prefs = getApplication()
+          .getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         String movieStr = prefs.getString("movie1","");
 
         try {
             JSONArray jsonArray = new JSONArray(movieStr);
             int len = jsonArray.length();
-            //Log.d("len", String.valueOf(len));
+
             for (int i=0; i<len; i++){
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
-                Log.d("bottomnav:json", jsonObject.toString(2));
+
                 String poster = jsonObject.getString("poster");
                 String title = jsonObject.getString("movie_title");
-                Log.d("bottomnav:movie_title", title);
-                int imgID = getApplication().getResources().getIdentifier(poster,"drawable",getApplication().getPackageName());
+                int imgID = getApplication().getResources()
+                  .getIdentifier(
+                    poster,
+                    "drawable",
+                    getApplication().getPackageName());
                 SimpleViewModel svm = new SimpleViewModel();
                 svm.setTitle(title);
                 svm.setPoster(poster);
                 svm.setImage(imgID);
                 movieList.add(svm);
-                Log.d("bottomnav:id", "" + imgID);
-                Log.d("bottomnav:movielist", movieList.toString());
+
             }
         } catch (JSONException e) {
             e.printStackTrace();
@@ -62,7 +59,6 @@ public class HomeViewModel extends AndroidViewModel {
         movieLiveData.setValue(movieList);
 
     }
-
     public LiveData<ArrayList<SimpleViewModel>> getArrayList() {
         return movieLiveData;
     }
