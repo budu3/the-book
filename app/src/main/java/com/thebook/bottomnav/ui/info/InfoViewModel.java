2601f6c8af9bb678f8ce1a2c771dc9a46178eb7f
budu3 <budu3@yahoo.com>
2024-11-09 17:14:42 -0600

    Add word wrap to InfoViewModel.java

--- app/src/main/java/com/thebook/bottomnav/ui/info/InfoViewModel.java
+++ app/src/main/java/com/thebook/bottomnav/ui/info/InfoViewModel.java
@@ -3,26 +3,26 @@ package com.thebook.bottomnav.ui.info;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.app.Application;
-
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 public class InfoViewModel extends AndroidViewModel {
-    // TODO: Implement the ViewModel
     private MutableLiveData<String> mText;
     private static String MY_PREFS_NAME = "filename";
 
     public InfoViewModel(@NonNull Application application) {
         super(application);
-        SharedPreferences prefs = getApplication().getSharedPreferences("movie", Context.MODE_PRIVATE);
+        SharedPreferences prefs = getApplication()
+          .getSharedPreferences("movie", Context.MODE_PRIVATE);
 
         mText = new MutableLiveData<>();
 
         SharedPreferences.Editor preferencesEditor = prefs.edit();
-        preferencesEditor.putString("description", "This is a dash board fragment");
-        //preferencesEditor.putInt("color", mCurrentColor);
+        preferencesEditor
+          .putString("description",
+            "This is a dash board fragment");
         preferencesEditor.apply();
 
         String description = prefs.getString("description", "");
