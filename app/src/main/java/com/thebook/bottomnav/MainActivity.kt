7f7a5b97c79b7c63a671d47254d707d42c4cf1af
budu3 <budu3@yahoo.com>
2024-11-10 23:54:09 -0600

    Update MainActivity.kt

--- app/src/main/java/com/thebook/bottomnav/MainActivity.kt
+++ app/src/main/java/com/thebook/bottomnav/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.thebook.bottomnav
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomnavigation
+    .BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -21,12 +22,15 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navController = findNavController(
+            R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
