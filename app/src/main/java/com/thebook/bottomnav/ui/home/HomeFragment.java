15e6ed8157603c7710dd6ff201374e3bf035fe4b
budu3 <budu3@yahoo.com>
2024-11-24 15:02:57 -0600

    Remove extranous logging

--- app/src/main/java/com/thebook/bottomnav/ui/home/HomeFragment.java
+++ app/src/main/java/com/thebook/bottomnav/ui/home/HomeFragment.java
@@ -26,8 +26,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class HomeFragment extends Fragment implements RecyclerViewAdapter.ItemClickListener {
-
-    //private HomeViewModel homeViewModel;
     private RecyclerView recyclerViewFav;
     private RecyclerView recyclerViewDrama;
     private RecyclerView recyclerViewComedy;
@@ -37,11 +35,14 @@ public class HomeFragment extends Fragment implements RecyclerViewAdapter.ItemCl
     private HomeFragment home;
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Initialize ViewModel
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
         Context context = getContext();
-        //homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
-        String url = "https://raw.githubusercontent.com/budu3/the-book/master/code/assets/images/";
+        String url =
+          "https://raw.githubusercontent.com/budu3/" +
+            "the-book/master/code/assets/images/";
 
         List<String> imageUrls = Arrays.asList(
           url+"a-ghost-story.jpg",
@@ -51,13 +52,15 @@ public class HomeFragment extends Fragment implements RecyclerViewAdapter.ItemCl
           url+"dark-tower.jpg");
 
         // Inflate the layout for this fragment
-        final View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final View root = inflater.inflate(R.layout.fragment_home,
+          container, false);
         home = this;
 
         // Set up RecyclerView for Favorites
         recyclerViewFav = root.findViewById(R.id.fav_recyclerview);
         recyclerViewFav.setHasFixedSize(true);
-        recyclerViewFav.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
+        recyclerViewFav.setLayoutManager(new LinearLayoutManager(context,
+          LinearLayoutManager.HORIZONTAL, false));
         adapterFav = new RecyclerViewAdapter(context, imageUrls);
         adapterFav.setClickListener(home);
         recyclerViewFav.setAdapter(adapterFav);
@@ -65,7 +68,9 @@ public class HomeFragment extends Fragment implements RecyclerViewAdapter.ItemCl
         // Set up RecyclerView for Drama
         recyclerViewDrama = root.findViewById(R.id.drama_recyclerview);
         recyclerViewDrama.setHasFixedSize(true);
-        recyclerViewDrama.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
+        recyclerViewDrama.setLayoutManager(
+          new LinearLayoutManager(context,
+            LinearLayoutManager.HORIZONTAL, false));
         adapterDrama = new RecyclerViewAdapter(context, imageUrls);
         adapterDrama.setClickListener(home);
         recyclerViewDrama.setAdapter(adapterDrama);
@@ -73,71 +78,21 @@ public class HomeFragment extends Fragment implements RecyclerViewAdapter.ItemCl
         // Set up RecyclerView for Comedy
         recyclerViewComedy = root.findViewById(R.id.comedy_recyclerview);
         recyclerViewComedy.setHasFixedSize(true);
-        recyclerViewComedy.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
+        recyclerViewComedy.setLayoutManager(
+          new LinearLayoutManager(context,
+            LinearLayoutManager.HORIZONTAL, false));
         adapterComedy = new RecyclerViewAdapter(context, imageUrls);
         adapterComedy.setClickListener(home);
         recyclerViewComedy.setAdapter(adapterComedy);
 
-        // Observe the ViewModel data to update RecyclerViews when data is available
-        /*
-        homeViewModel.getArrayList().observe(getViewLifecycleOwner(), new Observer<ArrayList<SimpleViewModel>>() {
-            @Override
-            public void onChanged(@Nullable ArrayList<SimpleViewModel> movieList) {
-                Context context = getContext();
-
-                Log.d("HomeFragment->",""+movieList.size());
-                if (context == null || movieList == null) {
-                    Log.d("HomeFragment->", "Context or movieList is null");
-                    return;
-                }
-
-                // Set up RecyclerView for Favorites
-                recyclerViewFav = root.findViewById(R.id.fav_recyclerview);
-                recyclerViewFav.setHasFixedSize(true);
-                recyclerViewFav.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
-                adapterFav = new RecyclerViewAdapter(context, movieList);
-                adapterFav.setClickListener(home);
-                recyclerViewFav.setAdapter(adapterFav);
-
-                // Set up RecyclerView for Drama
-                recyclerViewDrama = root.findViewById(R.id.drama_recyclerview);
-                recyclerViewDrama.setHasFixedSize(true);
-                recyclerViewDrama.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
-                adapterDrama = new RecyclerViewAdapter(context, movieList);
-                adapterDrama.setClickListener(home);
-                recyclerViewDrama.setAdapter(adapterDrama);
-
-                // Set up RecyclerView for Comedy
-                recyclerViewComedy = root.findViewById(R.id.comedy_recyclerview);
-                recyclerViewComedy.setHasFixedSize(true);
-                recyclerViewComedy.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
-                adapterComedy = new RecyclerViewAdapter(context, movieList);
-                adapterComedy.setClickListener(home);
-                recyclerViewComedy.setAdapter(adapterComedy);
-            }
-        });
-        */
         return root;
     }
 
     @Override
     public void onItemClick(View view, int position) {
-        /*
-        Bundle bundle = new Bundle();
-        SimpleViewModel item = adapterFav.getItem(position); // Assuming you’re working with fav_recyclerview
-
-        // Retrieve item details
-        int image = item.getImage();
-        String poster = item.getPoster();
-        String title = item.getTitle();
-
-        // Populate the bundle for navigation
-        bundle.putInt("image", image);
-        bundle.putString("poster", poster);
-        bundle.putString("title", title);
-        */
         // Display a Toast and navigate
-        Toast.makeText(getContext(), "You clicked on position " + position, Toast.LENGTH_SHORT).show();
-        //Navigation.findNavController(view).navigate(R.id.action_navigation_home_to_navigation_info, bundle);
+        Toast.makeText(getContext(),
+          "You clicked on position " + position, Toast.LENGTH_SHORT)
+          .show();
     }
 }
--- app/src/main/java/com/thebook/bottomnav/ui/home/RecyclerViewAdapter.java
+++ app/src/main/java/com/thebook/bottomnav/ui/home/RecyclerViewAdapter.java
@@ -1,7 +1,6 @@
 package com.thebook.bottomnav.ui.home;
 
 import android.content.Context;
-import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +10,6 @@ import android.widget.TextView;
 
 import com.thebook.bottomnav.R;
 
-import java.io.File;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,7 +17,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+public class RecyclerViewAdapter
+  extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
     private ArrayList<SimpleViewModel> data;
 
@@ -29,60 +27,32 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     private ItemClickListener mClickListener;
     private Context context;
 
-    /*
-    RecyclerViewAdapter(Context context, ArrayList<SimpleViewModel> data) {
-        this.layoutInflater = LayoutInflater.from(context);
-        this.data = data;
-        this.context = context;
-        Log.d("RecyclerViewAdapter->", "Inside RecyclerViewAdapter");
-    }
-    */
     RecyclerViewAdapter(Context context, List<String> images) {
         this.layoutInflater = LayoutInflater.from(context);
         this.images = images;
         this.context = context;
-        Log.d("RecyclerViewAdapter->", "Inside RecyclerViewAdapter");
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Log.d("RecyclerViewAdapter->", "Inside onCreateViewHolder");
-        View view = layoutInflater.inflate(R.layout.recyclerview_item, parent, false);
+        View view = layoutInflater.inflate(
+          R.layout.recyclerview_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        /*
-        Log.d("RecyclerView->","Inside onBindViewHolder");
-        //String pathToPicture = context.getCacheDir() + "/a-ghost-story.jpg";
-        SimpleViewModel movie = data.get(position);
-        String poster = movie.getPoster();
-        String pathToPicture = context.getCacheDir() + "/" + poster;
-        //holder.myImageView.setImageResource(movie.getImage());
-        File imgFile = new File(pathToPicture);
-        if (imgFile.exists()) {
-            holder.myImageView.setImageBitmap(BitmapFactory.decodeFile(pathToPicture));
-        } else {
-            Log.d("RecyclerViewAdapter->", "Image file does not exist at: " + pathToPicture);
-        }
-        holder.myImageView.setImageBitmap(BitmapFactory.decodeFile(pathToPicture));
-        Log.d("RecyclerView->",pathToPicture);
-
-         */
         String url = images.get(position);
         Glide.with(context).load(url).into(holder.myImageView);
     }
 
     @Override
     public int getItemCount() {
-        //Log.d("RecyclerViewAdapter->", "Data size in adapter: " + data.size());
-        //return data.size();
-        Log.d("RecyclerViewAdapter->", "Data size in adapter: " + images.size());
         return images.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class ViewHolder extends RecyclerView.ViewHolder
+      implements View.OnClickListener {
 
         TextView myTextView;
         ImageView myImageView;
@@ -96,15 +66,10 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         @Override
         public void onClick(View view) {
-            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+            if (mClickListener != null)
+                mClickListener.onItemClick(view, getAdapterPosition());
         }
     }
-
-    /*
-    SimpleViewModel getItem(int id) {
-        return data.get(id);
-    }
-     */
 
     void setClickListener(ItemClickListener itemClickListener) {
         this.mClickListener = itemClickListener;
