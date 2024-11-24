f6db932460d301795cf71736a3a000072ec958a5
budu3 <budu3@yahoo.com>
2024-11-24 15:18:02 -0600

    Add line wrap to HomeFragment.java file

--- app/src/main/java/com/thebook/bottomnav/ui/home/HomeFragment.java
+++ app/src/main/java/com/thebook/bottomnav/ui/home/HomeFragment.java
@@ -2,30 +2,23 @@ package com.thebook.bottomnav.ui.home;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.thebook.bottomnav.R;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class HomeFragment extends Fragment implements RecyclerViewAdapter.ItemClickListener {
+public class HomeFragment extends Fragment
+  implements RecyclerViewAdapter.ItemClickListener {
     private RecyclerView recyclerViewFav;
     private RecyclerView recyclerViewDrama;
     private RecyclerView recyclerViewComedy;
